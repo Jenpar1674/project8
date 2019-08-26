5d478e351187a5dbb3767e9bc4e4bdccb12a0f76
Jennifer Parker <jennifer.parker1@verizon.com>
2019-08-26 12:31:02 -0500

    added to app js

--- app.js
+++ app.js
@@ -48,5 +48,9 @@ const sequelize = new Sequelize({
 });
 // async IIFE
 (async () => {
-
-})();
+  try {
+    await sequelize.authenticate();
+    console.log('Connection to the database successful!');
+  } catch (error) {
+    console.error('Error connecting to the database: ', error);
+}})
