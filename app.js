7e70da87ea2710ae2f4fb7ce8211b6f11e655cd7
Jenpar1674 <51331242+Jenpar1674@users.noreply.github.com>
2019-08-26 20:48:24 -0500

    changes to files

--- app.js
+++ app.js
@@ -1,11 +1,12 @@
 var createError = require('http-errors');
 var express = require('express');
 var path = require('path');
+var methodOverride = require('method-override')
 var bodyParser = require('body-parser');
-var logger = require('morgan');
+
 
 var indexRouter = require('./routes/index');
-var usersRouter = require('./routes/users');
+//var usersRouter = require('./routes/users');
 
 var app = express();
 
@@ -13,14 +14,14 @@ var app = express();
 app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'pug');
 
-app.use(logger('dev'));
+app.use(bodyParser.urlencoded({ extended: false}));
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
-app.use(bodyParser());
+app.use(methodOverride('_method'));
 app.use(express.static(path.join(__dirname, 'public')));
 
 app.use('/', indexRouter);
-app.use('/users', usersRouter);
+//app.use('/users', usersRouter);
 
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
--- models/article.js
+++ models/article.js
@@ -2,7 +2,7 @@
 var dateFormat = require('dateformat');
 
 module.exports = function(sequelize, DataTypes) {
-  var Article = sequelize.define('Article', {
+  var Book = sequelize.define('Book', {
     title: {
       type: DataTypes.STRING,
       validate: {
@@ -11,8 +11,15 @@ module.exports = function(sequelize, DataTypes) {
         }
       }
     },
-    author: DataTypes.STRING,
-    body: DataTypes.TEXT
+     author: {
+      type: DataTypes.STRING,
+      validate: {
+        notEmpty: {
+          msg: "Author is required"
+        }
+      }
+  },
+   
   }, {
     classMethods: {
       associate: function(models) {
@@ -20,13 +27,10 @@ module.exports = function(sequelize, DataTypes) {
       }
     },
     instanceMethods: {
-      publishedAt: function() {
-        return dateFormat(this.createdAt, "dddd, mmmm dS, yyyy, h:MM TT");
+ 
       },
-      shortDescription: function(){ 
-        return this.body.length > 30 ? this.body.substr(0, 30) + "..." : this.body;
-      }
+     
     }
-  });
-  return Article;
+  );
+  return Book;
 };
