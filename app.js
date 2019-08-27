29737925462527e5eb37552e612d3c3b19d12ddd
Jenpar1674 <51331242+Jenpar1674@users.noreply.github.com>
2019-08-27 07:38:02 -0500

    mrnining

--- app.js
+++ app.js
@@ -1,13 +1,13 @@
-var createError = require('http-errors');
+// var createError = require('http-errors');
 var express = require('express');
 var path = require('path');
 
 var bodyParser = require('body-parser');
 
 
-var indexRouter = require('./routes/index');
+var routes = require('./routes/index');
 //var usersRouter = require('./routes/users');
-
+var books = require('./routes/books');
 var app = express();
 
 // view engine setup
@@ -15,13 +15,13 @@ app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'pug');
 
 app.use(bodyParser.urlencoded({ extended: false}));
-app.use(express.json());
-app.use(express.urlencoded({ extended: false }));
+app.use(bodyParser.json());
+
 
 app.use(express.static(path.join(__dirname, 'public')));
 
-app.use('/', indexRouter);
-//app.use('/users', usersRouter);
+app.use('/', routes);
+app.use('/books', books);
 
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
